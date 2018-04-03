f1465da0b17324372b3b466528353afb14187695
David Hunt <huntdog1541@gmail.com>
2018-04-02 23:09:37 -0500

    Added Register Class

--- VMquick1/Program.cs
+++ VMquick1/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace VMquick1
 {
     class Program
     {
+        private static List<String> code = new List<String>();
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            ReadFile("Test.txt");
+            Register e1 = new Register(1, "EAX", 32);
+            e1.printRegisterInformation();
+            Register e2 = new Register(2, "EBX", 256);
+            e2.printRegisterInformation();
+        }
+
+        public static void ReadFile(String FileName)
+        {
+            System.IO.StreamReader file = new System.IO.StreamReader(@"C:\Users\David\Source\Repos\VMquick1\VMquick1\Test\Test1.txt");
+            String line;
+            while((line = file.ReadLine()) != null)
+            {
+                code.Add(line);
+                System.Console.WriteLine(line);
+            }
+
         }
     }
 }
---
+++ VMquick1/Register.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VMquick1
+{
+    class Register
+    {
+        int RegisterID;
+        String RegisterName;
+        String RegisterLiteral;
+        int RegisterSize;
+        byte[] Storage;
+        int RegisterValue;
+
+        public Register(int id, String name, int val)
+        {
+            RegisterID = id;
+            RegisterName = name;
+            setValue(val);
+
+        }
+
+        public void setValue(int val)
+        {
+            RegisterValue = val;
+            Storage = BitConverter.GetBytes(RegisterValue);
+            Array.Reverse(Storage);
+        }
+
+        public String printRegisterStorage()
+        {
+            String s = string.Join(" ", Storage.Select(x => Convert.ToString(x, 2).PadLeft(8, '0')));
+            return s;
+        }
+
+        public void printRegisterInformation()
+        {
+            System.Console.WriteLine("Register Name: {0}", RegisterName);
+            System.Console.WriteLine("Register Literal {0}", RegisterLiteral);
+            System.Console.WriteLine("Register Value: {0}", RegisterValue);
+            System.Console.WriteLine("Register Storage: {0}", printRegisterStorage());
+        }
+    }
+}
